2185e57ee885f11bbbdc2bf18b9b6b2d977d554e
DigitalData <legoxavierlocketravers@gmail.com>
2026-06-18 23:07:43 +1000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 )
 
+const CMD_EXIT = "exit"
+
 func main() {
 	var name string
 	
 	for true {
 		fmt.Print("$ ")
 		fmt.Scanf("%s\n", &name)
+		if CMD_EXIT == name {
+			break
+		}
 		fmt.Printf("%s: command not found\n", name)
 	}
 	
